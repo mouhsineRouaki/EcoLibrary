e5a54cb9b62ef7b32fdecae167078a1e2850086a
Mouhsine Rouaki <houtm27@gmail.com>
2026-03-10 14:11:55 +0000

    feat(factory): add database/factories/BookViewFactory.php

---
+++ database/factories/BookViewFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BookView>
+ */
+class BookViewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'book_id' => Book::factory(),
+            'user_id' => fake()->boolean(70) ? User::factory() : null,
+            'viewed_at' => fake()->dateTimeBetween('-6 months', 'now'),
+        ];
+    }
+}
