eaf479ae1c4ff768d8f1631b16bd68b22844a780
Mouhsine Rouaki <houtm27@gmail.com>
2026-03-10 14:11:56 +0000

    feat(db): add database/migrations/2026_03_09_140701_create_books_table.php

---
+++ database/migrations/2026_03_09_140701_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->string('author');
+            $table->integer('total_quantity')->default(0);
+            $table->integer('available_quantity')->default(0);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->index('title');
+            $table->index('author');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
